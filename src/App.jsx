a87b8efe2a225b1be8ef80d07d8f115e2fa9d988
hendra solih <hendra.solih.jp@gmail.com>
2024-02-15 21:19:38 +0700

    lift up logic game board componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import Player from './components/Player.jsx';
-import GameBoard from './components/GameBoard.jsx';
-import { useState } from 'react';
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
+import { useState } from "react";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTruns, setGameTruns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
-    setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X')
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+    setGameTruns((prevTruns) => {
+      let currentPlayer = "X";
+
+      if (prevTruns.length > 0 && prevTruns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTruns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTruns} />
       </div>
     </main>
   );
